50827967aafa599f95d3111f954b4093564c2bbe
DevSanBee <sanusiolaniyan@gmail.com>
2023-09-26 12:32:47 +0000

    Removed unnecessary comments

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,3 @@
-// In 0-constants.js
-
-// Modify taskFirst to use const
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -10,7 +7,6 @@ export function getLast() {
   return ' is okay';
 }
 
-// Modify taskNext to use let
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
